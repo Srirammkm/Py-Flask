44e06c78b5afb58cf7fbf24d446a29b3dd715eca
Srirammkm <35733243+Srirammkm@users.noreply.github.com>
2021-03-05 07:41:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 import os
+import mysql.connector
 from flask import Flask, render_template, request
-from flask_mysqldb import MySQL
 app = Flask(__name__)
+mydb = mysql.connector.connect(
+  host="54.254.185.134",
+  user="root",
+  password="root",
+  database="MyDB"
+)
 
-
-app.config["MYSQL_DATABASE_USER"] = "root"
-app.config["MYSQL_DATABASE_PASSWORD"] = "root"
-app.config["MYSQL_DATABASE_DB"] = "MyDB"
-app.config["MYSQL_DATABASE_HOST"] = os.getenv("MYSQL_SERVICE_HOST")
-app.config["MYSQL_DATABASE_PORT"] = int(os.getenv("MYSQL_SERVICE_PORT"))
-
-mysql = MySQL(app)
-
+mycursor = mydb.cursor()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -19,11 +17,10 @@ def index():
         details = request.form
         firstName = details['fname']
         lastName = details['lname']
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO MyUsers(firstName, lastName) VALUES (%s, %s)", (firstName, lastName))
-        mysql.connection.commit()
-        cur.close()
-        return 'success'
+        val = [(firstName, lastName)]
+        sql = "INSERT INTO MyUsers(firstName, lastName) VALUES (%s, %s)"
+        mycursor.executemany(sql, val)
+        mydb.commit()
     return render_template('index.html')
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
